4009a10ca0326afaa0824175f43119081d736e74
Jim Doyle <indellibleink@gmail.com>
2019-11-28 22:47:32 -0500

    testing a card flip.

--- GoFish/Program.cs
+++ GoFish/Program.cs
@@ -6,9 +6,18 @@ namespace GoFish
     {
         static void Main(string[] args)
         {
-            Card card = new Card();
+            Card card = new Card(11,0);
             
+            switch(card.Value)
+            {
+                case 11:
+                    Console.WriteLine("{0}{1}",card.RoyalFace(0), card.SuitSym());
+                    break;
+                default:
+                    Console.WriteLine("{0}{1}", card.Value, card.SuitSym());
+                    break;
 
+            }
         }
     }
 }
