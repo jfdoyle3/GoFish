b3a162634a84df23ec76f4c77bda5f183a657e50
Jim Doyle <indellibleink@gmail.com>
2019-12-05 00:01:47 -0500

    trying to get face card to display

--- GoFish/Card.cs
+++ GoFish/Card.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+using System;
 using System.Collections.Generic;
 
 namespace GoFish
@@ -9,10 +10,15 @@ namespace GoFish
         public int Suit;
         private string faceDown = "##";
         private string[] Suits = { "S", "H", "C", "D" };
-        private string[] royalFace = { "J", "Q", "K"};
+       // private string[] royalFace = { "J", "Q", "K"};
         private object[] joker = { "j1", "j2",15,16 };
         private object[] ace = { "A", 1, 11, 14 };
-
+        private Dictionary<int, string> royalFace = new Dictionary<int, string>()
+        {
+            {11,"J"},
+            {12,"Q"},
+            {13,"K"}
+        };
 
         public Card()
         {
@@ -44,28 +50,30 @@ namespace GoFish
         {
             return this.ace[0].ToString();
         }
-        public static void Display(int value)
+        public void Display()
         {
-            switch (value)
-            {
-                case 11:
-                    // Console.WriteLine("{0}{1}", this.RoyalFace(0), this.SuitSym());
-                    return RoyalFace(0);
-                    break;
-                case 12:
-                    Console.WriteLine("{0}{1}", this.RoyalFace(1), this.SuitSym());
-                    break;
-                case 13:
-                    Console.WriteLine("{0}{1}", this.RoyalFace(2), this.SuitSym());
-                    break;
-                case 14:
-                case 1:
-                    Console.WriteLine("{0}{1}", this.Ace(), this.SuitSym());
-                    break;
-                default:
-                    Console.WriteLine("{0}{1}", this.Value, this.SuitSym());
-                    break;
-            }
+            //string output;
+           // return maps.TryGetValue(royalFace, out output) ? output : "default";
+            //switch (value)
+            //{
+            //    case 11:
+            //         Console.WriteLine("{0}{1}", this.RoyalFace(0), this.SuitSym());
+            //        //return RoyalFace(0);
+            //        break;
+            //    case 12:
+            //        Console.WriteLine("{0}{1}", this.RoyalFace(1), this.SuitSym());
+            //        break;
+            //    case 13:
+            //        Console.WriteLine("{0}{1}", this.RoyalFace(2), this.SuitSym());
+            //        break;
+            //    case 14:
+            //    case 1:
+            //        Console.WriteLine("{0}{1}", this.Ace(), this.SuitSym());
+            //        break;
+            //    default:
+            //        Console.WriteLine("{0}{1}", this.Value, this.SuitSym());
+            //        break;
+            //}
         }
     }
 }
