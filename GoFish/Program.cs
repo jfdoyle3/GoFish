1aeed9dfe7efac1ded58daf4a2a614a1a4e2253e
Jim Doyle <indellibleink@gmail.com>
2019-12-15 11:13:14 -0500

    removed commented test code from master

--- GoFish/Program.cs
+++ GoFish/Program.cs
@@ -14,31 +14,7 @@ namespace GoFish
             ////////////////////
 
            
-            //List<Card> h = new List<Card>();
-            //  Deck deck = new Deck();
-            // var shuffled=deck.Shuffle();
-            //var topCards = shuffled.Take(5);
-            //Console.WriteLine("Top 5 cards");
-
-            //foreach (var cards in topCards)
-            //{
-            //    Console.WriteLine("{0}{1}",cards.Value,cards.SuitSym());
-            //}
-
-            //Console.WriteLine("removing top card");
-            //var topCard = shuffled[0];
-            //shuffled.RemoveAt(0);
-            //h.Add(topCard);
-
-
-            //Console.WriteLine("Card in H is {0}{1}",h[0].Value,h[0].SuitSym());
-            //Console.WriteLine("new top of deck");
-
-            //foreach (var cards in topCards)
-            //{
-            //    Console.WriteLine("{0}{1}", cards.Value, cards.SuitSym());
-            //}
-
+          
         }
     }
 }
